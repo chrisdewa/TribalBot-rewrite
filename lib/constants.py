67c9b3d0fc82ce37b7aec91a8826a5d4e722ae8b
ChrisDewa <christian.delaflor@gmail.com>
2022-08-20 21:14:05 -0500

    Added dotenv variable loading

--- lib/constants.py
+++ lib/constants.py
@@ -1,2 +1,11 @@
+import os
 
-TOKEN = '' # TODO: fill from .env
+from dotenv import load_dotenv
+load_dotenv()
+
+__all__ = [
+    'BOT_TOKEN',
+    'DATABASE_URL',
+]
+BOT_TOKEN = os.getenv('BOT_TOKEN')
+DATABASE_URL = os.getenv('DATABASE_URL')
