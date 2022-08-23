d066651168a6158239623aa973359db166390282
ChrisDewa <christian.delaflor@gmail.com>
2022-08-22 21:33:59 -0500

    improved logic with less database calls

--- lib/controllers/tribes.py
+++ lib/controllers/tribes.py
@@ -3,17 +3,15 @@ from discord import Guild, Member
 
 from lib.orm.models import Tribe, GuildConfig
 
-async def get_guild_tribes(guild: Guild) -> list[Tribe]:
-    guild_config, _ = await GuildConfig.get_or_create(guild_id=guild.id)
-    tribes = await guild_config.tribes.all()
-    return tribes
+async def get_guild_tribes(guild: Guild) -> set[Tribe]:
+    tribes = await Tribe.filter(guild_config=guild.id)
+    return set(tribes)
 
-async def get_member_tribes(member: Member) -> list[Tribe]:
-    guild = member.guild
-    guild_config, _ = await GuildConfig.get_or_create(guild_id=guild.id)
+async def get_member_tribes(member: Member) -> set[Tribe]:
+    guild_id = member.guild.id
     tribes = (
-        await Tribe.filter(guild_config=guild_config, leader=member.id) +
-        await Tribe.filter(guild_config=guild_config, members__contains=member.id)
+        await Tribe.filter(guild_config=guild_id, leader=member.id) +
+        await Tribe.filter(guild_config=guild_id, members__contains=member.id)
     )
-    return tribes
+    return set(tribes)
 
