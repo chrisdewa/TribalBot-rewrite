53bbd7e96ef2e684a4175152b145b0e80c149ad3
ChrisDewa <christian.delaflor@gmail.com>
2022-08-23 20:23:12 -0500

    added cog load logic

--- lib/bot/bot.py
+++ lib/bot/bot.py
@@ -1,3 +1,9 @@
+"""
+Contains most of the bot setups
+Loads cogs, prepares, starts and closes the database.
+"""
+from pathlib import Path
+
 from discord import Bot
 
 from lib.constants import BOT_TOKEN as TOKEN
@@ -16,8 +22,19 @@ class TribalBot(Bot):
             f'{"="*30}'
         )
     
+    def load_cogs(self):
+        cogs = Path(__file__).parent / 'cogs' # absolute location of the cogs module
+        base_path = 'lib.bot.cogs.'
+        
+        for cog in cogs.glob('*.py'): # iterate over the python files in the cogs directory
+            if not cog.name.startswith('_'): # skip those starting with lower dash
+                self.load_extension(base_path + cog.name[:-3]) # join the base path with the 
+                                                               # name of the cog without the extension
+    
     def run(self) -> None:
         print('[!] Starting bot')
+        print('[!] Loading cogs')
+        self.load_cogs()
         print('[!] initializing database')
         self.loop.run_until_complete(init_db())
         try:
