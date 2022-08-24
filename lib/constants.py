8904aff0731b68bee6d32b8e4e99823ff9d20aa7
ChrisDewa <christian.delaflor@gmail.com>
2022-08-23 20:22:53 -0500

    added default tribe color as constant

--- lib/constants.py
+++ lib/constants.py
@@ -8,10 +8,15 @@ __all__ = [
     'BOT_TOKEN',
     'DATABASE_URL',
     'guild_ids',
+    'DEFAULT_TRIBE_COLOR',
 ]
 
-DEV_MODE = os.getenv('DEV_MODE', False)
+DEFAULT_TRIBE_COLOR = 16711680
+
+DEV_MODE = os.getenv('DEV_MODE', False)  # defines amongs other things if commands should be synced for a single server (development server)
+if DEV_MODE:
+    print('[!] Bot running in development mode')
 BOT_TOKEN = os.getenv('BOT_TOKEN')
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-guild_ids = None if DEV_MODE else [716844885457764435]
+guild_ids = None if DEV_MODE else [716844885457764435] # if DEV_MODE is not enabled commands will be available globally
