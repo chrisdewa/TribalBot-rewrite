f569b943e0bf44fb6517bd68f821d5a12e9a9970
ChrisDewa <christian.delaflor@gmail.com>
2022-08-25 09:33:00 -0500

    leader role config starter

--- lib/bot/cogs/configs.py
+++ lib/bot/cogs/configs.py
@@ -1,8 +1,8 @@
 from discord.ext.commands import Cog
-from discord import app_commands, Interaction
+from discord import app_commands, Interaction, permissions, Role
 
 from lib.bot import TribalBot
-from lib.controllers.configs import create_new_category
+from lib.controllers.configs import create_new_category, set_leaders_role
 
 class ConfigurationsCog(Cog, description='Tribe and guild configuration commands'):
     def __init__(self, bot) -> None:
@@ -12,16 +12,31 @@ class ConfigurationsCog(Cog, description='Tribe and guild configuration commands
     async def cog_unload(self) -> None:
         print(f'[-] {self.qualified_name} unloaded')
     
+    @app_commands.command(name='select-tribe-leader', description='Selects the leaders role. [Caution] Overwrites previous setting.')
+    @app_commands.describe(name='The name of the category')
+    @app_commands.guild_only()
+    @app_commands.checks.has_permissions(manage_guild=True, manage_roles=True)
+    async def select_tribe_leader(
+        self,
+        itr: Interaction,
+        role: Role,
+    ):
+        await set_leaders_role(itr.guild, role)
+        await itr.response.send_message(f'Done! The new Leaders role is {role.mention}', ephemeral=True)
+
+    
+    
     @app_commands.command(name='category-create', description='Creates a new tribe category')
     @app_commands.describe(name='The name of the category')
     @app_commands.guild_only()
+    @app_commands.checks.has_permissions(manage_guild=True)
     async def category_create_command(
         self, 
-        interaction: Interaction,
+        itr: Interaction,
         name: app_commands.Range[str, 5, 30],
     ):
-        cat = await create_new_category(interaction.guild, name=name)
-        await interaction.response.send_message(f'The category "{name}" was successfully created with id {cat.pk}', ephemeral=True)
+        cat = await create_new_category(itr.guild, name=name)
+        await itr.response.send_message(f'The category "{name}" was successfully created with id {cat.pk}', ephemeral=True)
 
 async def setup(bot: TribalBot):
     await bot.add_cog(ConfigurationsCog(bot))
--- lib/controllers/configs.py
+++ lib/controllers/configs.py
@@ -1,5 +1,5 @@
 
-from discord import Guild
+from discord import Guild, Role
 from lib.orm.models import GuildConfig, TribeCategory
 
 async def get_guild_config(guild: Guild) -> GuildConfig:
@@ -12,3 +12,15 @@ async def create_new_category(guild: Guild, name: str) -> TribeCategory:
     if same_categories:
         raise ValueError(f'Tribe category with name "{name}" already exists')
     return await TribeCategory.create(guild_config=guild_config, name=name)
+
+async def set_leaders_role(guild: Guild, role: Role) -> None:
+    """sets up the leaders role in the guild configurations
+
+    Args:
+        guild (Guild): the guild to alter
+        role (Role): the role to set
+    """
+    guild_config = await get_guild_config(guild)
+    guild_config.leaders_role = role.id
+    await guild_config.save()
+    
