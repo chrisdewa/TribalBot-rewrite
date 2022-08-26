8f14e64ad5fa206a5439df52a52a18f86e1ec71d
ChrisDewa <christian.delaflor@gmail.com>
2022-08-25 21:53:10 -0500

    added check for guild leaders role

--- lib/utils/checks.py
+++ lib/utils/checks.py
@@ -1,7 +1,29 @@
 from discord import Interaction, app_commands
 
+from lib.controllers.configs import get_guild_config
+from lib.orm.models import GuildConfig
+
 def is_chrisdewa():
     """Allows an app command to run only if the user is ChrisDewa#4552"""
     def predicate(interaction: Interaction) -> bool:
         return interaction.user.id == 365957462333063170
+    return app_commands.check(predicate)
+
+def guild_has_leaders_role():
+    """Blocks commands unless the server admins have set up the leader's role"""
+    async def predicate(interaction: Interaction) -> bool:
+        print('this is running')
+        guild = interaction.guild
+        if not guild: return False
+        print('there is a guild')
+        guild_config = await get_guild_config(guild)
+        if not guild_config.leaders_role or not guild.get_role(guild_config.leaders_role):
+            await interaction.response.send_message(
+                'The command failed because the leaders role is not configured, talk to your server admins', 
+                ephemeral=True
+            )
+            return False
+        else:
+            return True
+        
     return app_commands.check(predicate)
